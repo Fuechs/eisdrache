58a138a7387013fc32d8a1a7bb7f0a67a1a145c5
Füchs <68223337+Fuechs@users.noreply.github.com>
2023-01-31 16:13:18 +0100

    Update Wrapper

--- wrapper/wrapper.cpp
+++ wrapper/wrapper.cpp
@@ -54,7 +54,34 @@ LLVMContext *Wrapper::getContext() { return context; }
 Module *Wrapper::getModule() { return module; }
 IRBuilder<> *Wrapper::getBuilder() { return builder; }
 
+IntegerType *Wrapper::getSizeTy() { return getIntTy(64); }
 IntegerType *Wrapper::getIntTy(size_t bit) { return Type::getIntNTy(*context, bit); }
 PointerType *Wrapper::getIntPtrTy(size_t bit) { return Type::getIntNPtrTy(*context, bit); }
+PointerType *Wrapper::getIntPtrPtrTy(size_t bit) { return getIntPtrTy(bit)->getPointerTo(); }
+
+
+/// BUILDER ///
+Value *Wrapper::allocate(Type *type, std::string name) { return builder->CreateAlloca(type, nullptr, name); }
+Value *Wrapper::call(Function *function, std::vector<Value *> args, std::string name) { return builder->CreateCall(function, args, name); };
+Function *Wrapper::declare(Type *type, std::vector<Type *> parameters, std::string name) {
+    FunctionType *FT = FunctionType::get(type, parameters, false);
+    Function *F = Function::Create(FT, Function::ExternalLinkage, name, *module);
+    llvm::verifyFunction(*F);
+    return F;
+}
+
+Value *Wrapper::malloc(Type *type, Value *size, std::string name) { return call(memoryFunctions[type]["malloc"], {size}, name); }
+void Wrapper::free(Type *type, Value *value) { call(memoryFunctions[type]["free"], {value}); }
+Value *Wrapper::memcpy(Type *type, Value *dest, Value *source, Value *size, std::string name) { return call(memoryFunctions[type]["memcpy"], {dest, source, size}, name); }
+
+void Wrapper::createMemoryFunctions(Type *type) {
+    PointerType *ptrType = type->getPointerTo();
+    memoryFunctions[type]["malloc"] = Function::Create(FunctionType::get(ptrType, {getSizeTy()}, false), Function::ExternalLinkage, "malloc", *module);
+    memoryFunctions[type]["free"] = Function::Create(FunctionType::get(builder->getVoidTy(), {ptrType}, false), Function::ExternalLinkage, "free", *module);
+    memoryFunctions[type]["memcpy"] =  Function::Create(FunctionType::get(ptrType, {ptrType, ptrType, getSizeTy()}, false), Function::ExternalLinkage, "memcpy", *module);
+    llvm::verifyFunction(*memoryFunctions[type]["malloc"]);
+    llvm::verifyFunction(*memoryFunctions[type]["free"]);
+    llvm::verifyFunction(*memoryFunctions[type]["memcpy"]);
+}
 
 }
--- wrapper/wrapper.hpp
+++ wrapper/wrapper.hpp
@@ -12,6 +12,8 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <map>
 
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
@@ -40,9 +42,24 @@ public:
     LLVMContext *getContext();
     Module *getModule();
     IRBuilder<> *getBuilder();
-
+    IntegerType *getSizeTy();
     IntegerType *getIntTy(size_t);
     PointerType *getIntPtrTy(size_t);
+    PointerType *getIntPtrPtrTy(size_t);
+
+    // builder functions
+    Value *allocate(Type *, std::string = "");
+    Value *call(Function *, std::vector<Value *> = {}, std::string = "");
+    Function *declare(Type *, std::vector<Type *> = {}, std::string = "");
+
+    // call TYPE *malloc (SIZE_T size)
+    Value *malloc(Type *, Value *, std::string = "");
+    // call void free (TYPE *value)
+    void free(Type *, Value *);
+    // call TYPE *memcpy (TYPE *dest, TYPE *source, SIZE_T size)
+    Value *memcpy(Type *, Value *, Value *, Value *, std::string = "");
+
+    void createMemoryFunctions(Type *);
 
 private:
     Wrapper(LLVMContext *, Module *, IRBuilder<> *);
@@ -52,6 +69,7 @@ private:
     IRBuilder<> *builder;
 
     Function *main = nullptr;
+    std::map<Type *, std::map<std::string, Function *>> memoryFunctions = std::map<Type *, std::map<std::string, Function *>>();
 };
 
 }
